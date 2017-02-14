c1f91a2548e0d7d4aeb4f10da73d7b9d54c0224b
Nakata <kojinktganbaoru2012@gmail.com>
2017-02-14 09:12:22 +0900

    add template

---
+++ main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 13 22:53:36 2017
+
+@author: Koji
+"""
+
+import cv2
+import numpy as np
+
+import modelmaker
+
+filename = "test.jpg"
+
+img = cv2.imread(filename, 0)
+cv2.imshow("img", img)
+cv2.waitKey(-1)
+
+modelmaker.createModel(img)
---
+++ modelmaker.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 13 23:01:14 2017
+
+@author: Koji
+"""
+
+import numpy as np
+
+def createModel(img):
+    """
+    画像から3次元モデルを生成する
+
+    Parameters
+    -----
+    img (ndarray)
+        対象の画像
+    
+    Returns
+    -----
+    
+    """
+    
+    return
